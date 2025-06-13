5878baddb7564a068a8c80524fa73358d7890323
Felipe Alencar <gia.comini@hotmail.com>
2025-06-13 14:02:50 -0300

    Init project

---
+++ src/main/java/com/report/report_api_pattern/ReportApiPatternApplication.java
@@ -0,0 +1,13 @@
+package com.report.report_api_pattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportApiPatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportApiPatternApplication.class, args);
+	}
+
+}
